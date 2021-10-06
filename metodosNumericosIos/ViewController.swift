05cb3e7963a2ed1f6e8de909a4ba0a153572b7d3
Jhonatan Chavez Chavez <jchavech@everis.com>
2021-10-06 14:42:03 -0500

    creacion de Proyecto

---
+++ metodosNumericosIos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  metodosNumericosIos
+//
+//  Created by Jhonatan Chavez Chavez on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
